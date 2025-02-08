812bf908b3707cd9145ee72a7763795b7293e9cc
Ami Lian <amiamilian99@gmail.com>
2025-02-07 21:43:06 -0500

    Adding DOM

---
+++ Week 4 JS+HTML Game/scripts.js
@@ -0,0 +1,14 @@
+/****************************
+ * DOM Element References
+ ****************************/
+const rat = document.getElementById('rat');
+const gamedisplay = document.querySelector('.gamedisplay');
+const currentScore = document.getElementById('current');
+const totalScoreElement = document.getElementById('total');
+const restartBtn = document.getElementById('restart');
+const menuBtn = document.getElementById('menu');
+const startScreen = document.getElementById('start-screen');
+const menuPopup = document.getElementById('menu-popup');
+const buyFreedomBtn = document.getElementById('buy-freedom');
+const closePopupBtn = document.getElementById('close-popup');
+const coin = document.getElementById('coin');
