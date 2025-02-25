4c2a61962a5f096f65989e6aed84d09c268c5cc9
Ami Lian <amiamilian99@gmail.com>
2025-02-24 19:41:52 -0500

    Set timer

--- Week 5 React Game/amis-gomoku/src/components/Board.js
+++ Week 5 React Game/amis-gomoku/src/components/Board.js
@@ -11,6 +11,32 @@ const GomokuBoard = () => {
   const [gameStarted, setGameStarted] = useState(false);
   const [winner, setWinner] = useState(null);
 
+  useEffect(() => {
+    if (!gameStarted || winner) return;
+
+    const timer = setInterval(() => {
+      activePlayer === 'black'
+        ? setBlackTime(prevTime =>
+            prevTime <= 1
+              ? (setWinner('W'), shootConfetti(), 0)
+              : prevTime - 1
+          )
+        : setWhiteTime(prevTime =>
+            prevTime <= 1
+              ? (setWinner('B'), shootConfetti(), 0)
+              : prevTime - 1
+          );
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [activePlayer, gameStarted, winner]);
+
+  const formatTime = time => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+  
   return (
     <div className="main">
       <div className="gomoku-board">
