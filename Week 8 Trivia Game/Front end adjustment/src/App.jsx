3231bae03ab01ae0cdd8fc46f364f5f7bcbf9bbf
Ami Lian <amiamilian99@gmail.com>
2025-03-07 14:47:58 -0500

    Remove Qs that answers none of above, with multiple answers, and answers with all of the above for T/F

--- Week 8 Trivia Game/Front end adjustment/src/App.jsx
+++ Week 8 Trivia Game/Front end adjustment/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div className="transparent-box">
+      <div className="main-box">
       </div>
     </div>
   );
