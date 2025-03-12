de4f8eb2d05236549963afb4c086a691a5635fb8
Ami Lian <amiamilian99@gmail.com>
2025-03-12 17:52:18 -0400

    Add greeting to navbar

--- Week 9 Eventonica/client/src/components/Navbar.jsx
+++ Week 9 Eventonica/client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     return (
     <nav className="navbar">
         <h2 className="header">Eventonica</h2>
+        <h3 className="greeting">Hello, _____  !</h3>
         <ul className="navbar-list">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/events">Manage Events</Link></li>
