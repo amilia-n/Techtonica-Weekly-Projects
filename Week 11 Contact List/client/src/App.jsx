1c754146234d73b738a39633854a326716ba0c2d
Ami Lian <amiamilian99@gmail.com>
2025-03-25 18:38:21 -0400

    inital setup for Contact App

---
+++ Week 11 Contact List/client/src/App.jsx
@@ -0,0 +1,14 @@
+// Root component of the application, rendering the Home page
+import './App.css';
+// import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      {/* <Home /> */}
+      Pretend this is home
+    </div>
+  );
+}
+
+export default App;
